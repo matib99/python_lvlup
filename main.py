6e315fbeb98aa832abf3bbf97e891e7163efe01a
Mateusz Błajda <mati99.b@gmail.com>
2020-04-04 00:10:22 +0200

    zad2

--- main.py
+++ main.py
@@ -5,3 +5,19 @@ app = FastAPI()
 @app.get("/")
 def root():
     return {"message": "Hello World during the coronavirus pandemic!"}
+
+@app.get("/method")
+def method_get():
+    return {"method": "GET"}
+
+@app.post("/method")
+def method_post():
+    return {"method": "POST"}
+
+@app.put("/method")
+def method_put():
+    return {"method": "PUT"}
+
+@app.delete("/method")
+def method_del():
+    return {"method": "DELETE"}
