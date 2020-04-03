633b130472a5bd5e8f3fc7a646990c7c7fc75c49
Mateusz Błajda <mati99.b@gmail.com>
2020-04-04 00:34:13 +0200

    zad3

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class Patient(BaseModel):
+    name: str
+    surname: str
+
+app.id = 0
 
 @app.get("/")
 def root():
     return {"message": "Hello World during the coronavirus pandemic!"}
+
+@app.post("/patient")
+def patient(patient: Patient):
+    app.id += 1
+    return {"id": (app.id - 1), "patient": patient}
 
 @app.get("/method")
 def method_get():
@@ -21,3 +33,5 @@ def method_put():
 @app.delete("/method")
 def method_del():
     return {"method": "DELETE"}
+
+
