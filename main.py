6472e0a7de221fba76f547e570f7ad3ba07de711
Mateusz Błajda <mati99.b@gmail.com>
2020-04-27 01:06:03 +0200

    asdasdsa

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
-from fastapi import FastAPI, HTTPException, Request, Response, status
+from fastapi import FastAPI, HTTPException, Request, Depends, Cookie, Response, status
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
 from pydantic import BaseModel
+from hashlib import sha256
+
+import secrets
 
 app = FastAPI()
+
+app.secret_key = "very constant and random secret, best sixty four (64) characters"
+app.tokens_list = []
+security = HTTPBasic()
 
 class Patient(BaseModel):
     name: str
@@ -9,6 +17,20 @@ class Patient(BaseModel):
 app.countRoot = 0
 app.countWelc = 0
 app.patients = []
+
+def log_in_and_get_token(credentials: HTTPBasicCredentials = Depends(security)):
+    correct_username = secrets.compare_digest(credentials.username, "trudnY")
+    correct_password = secrets.compare_digest(credentials.password, "PaC13Nt")
+    if not (correct_username and correct_password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect email or password",
+            headers={"WWW-Authenticate": "Basic"},
+        )
+    token = sha256(bytes(f"{credentials.username}{credentials.password}{app.secret_key}", encoding='utf8')).hexdigest()
+    app.tokens_list.append(token)
+    return token
+
 
 @app.get("/")
 def root():
@@ -24,12 +46,12 @@ def welcome():
     
 
 @app.post("/patient")
-def patient(patient: Patient):
+def new_patient(patient: Patient):
     app.patients.append(patient)
     return {"id": (len(app.patients) - 1), "patient": patient}
 
 @app.get("/patient/{pk}", response_model=Patient)
-def patient(pk: int):
+def get_patient(pk: int):
     if pk >= len(app.patients) or pk < 0:
         raise HTTPException(status_code=204, detail="Patient not found")
     return app.patients[pk]
@@ -37,6 +59,14 @@ def patient(pk: int):
 # @app.api_route(path="/method", methods=["GET", "POST", "DELETE", "PUT", "OPTIONS"])
 # def read_request(request: Request):
 #     return {"method": request.method}
+
+@app.post("/login")
+def login(request: Request, credentials_user = Depends(log_in_and_get_token)):
+    printf(credentials_user)
+    response = RedirectResponse(url = "/welcome")
+    response.status_code = status.HTTP_302_FOUND
+    return response
+
 
 @app.get("/clear")
 def method_del():
